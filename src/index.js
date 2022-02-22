f1a160f8ad0d02bec4e30b3e213b7defe0aece7e
DPreethi145 <dondapatipreethi145@gmail.com>
2022-02-22 18:43:10 +0530

    firs

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Heading from './Heading'
+
+ReactDOM.render(
+  <>
+   <h1 className='head'>STYLING USING FUNCTIONAL AND CLASS COMPONENTS</h1>
+ 
+   <Heading />
+   <App />
+   </>,
+  document.getElementById('root')
+);
